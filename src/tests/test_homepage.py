da9570372bb201ababaa7e88e7c1c1e2b965cf37
asubaruwrxsti <arlind12344@gmail.com>
2025-02-21 15:16:54 +0100

    Updated unit testing

---
+++ src/tests/test_homepage.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_home_endpoint():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+def test_home_endpoint_wrong_method():
+    response = client.post("/")
+    assert response.status_code == 405
+
+def test_home_endpoint_headers():
+    response = client.get("/")
+    assert "application/json" in response.headers["content-type"]
